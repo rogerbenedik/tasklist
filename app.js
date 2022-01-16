09a842c5b3db510bc12833969231b50511451647
rogerio <rogerbenedik@hotmail.com>
2022-01-16 18:46:17 -0400

    feat: added task if null send alert

---
+++ app.js
@@ -0,0 +1,41 @@
+// define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-taks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+// make load all event listerners
+loadEventListeners();
+// load all event list.
+function loadEventListeners(){
+//add task event
+form.addEventListener('submit', addTask);
+}
+// add task
+
+function addTask(e) {
+
+if(taskInput.value === '') {
+    alert('Please add a task!');
+}
+
+//create li element
+const li = document.createElement('li');
+li.className ='collection-item';
+//create textn node and append to li
+li.appendChild(document.createTextNode(taskInput.value));
+//create new Link element
+const link = document.createElement('a');
+//add class
+link.className = 'delete-item secondary-content';
+//add icon html
+link.innerHTML = '<i class="fas fa-window-close"></i>';
+// append the link to li
+li.appendChild(link);
+//append li to ul
+taskList.appendChild(li);
+//clear input
+taskInput.value = '';
+
+    e.preventDefault();
+}
